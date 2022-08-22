d173fd7a36ee9d606ef44aaa99880795383b6961
Tim Ruser <tim@ruser.dev>
2022-08-22 22:23:28 +0200

    Added dotenv and connection functionality

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// Read env variables
+require('dotenv').config()
+
+// Connect to local MySQL server
 var mysql = require('mysql')
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD
   });
   
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log(`Connected to ${process.env.MYSQL_HOST} as user ${process.env.MYSQL_USERNAME}!`);
+});
