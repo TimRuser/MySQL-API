e68fdab53263c3b1b75cb947190888a2d61c27d1
Tim Ruser <tim@ruser.dev>
2022-08-22 22:32:10 +0200

    Added api

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ con.connect(function(err) {
   if (err) throw err;
   console.log(`Connected to ${process.env.MYSQL_HOST} as user ${process.env.MYSQL_USERNAME}!`);
 });
+
+// Start API
+const express = require('express');
+const app = express();
+
+// Allows cross origin requests
+const cors = require('cors');
+app.use(cors({ origin: true }));
+
+
+app.use(express.json());
+
+
+app.post('/test', (req, res) => {
+  res.status(200).send({ received_body: req.body });
+});
+
+// Start the API with Express
+const port = process.env.API_PORT || 3333;
+app.listen(port, () => console.log(`API available on http://localhost:${port}`));
