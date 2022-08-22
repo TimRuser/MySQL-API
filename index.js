e2f438a0cb136c15933d0bc6780990dc49a3e177
Tim Ruser <tim@ruser.dev>
2022-08-22 22:58:42 +0200

    Added password generator

--- index.js
+++ index.js
@@ -15,17 +15,38 @@ con.connect(function(err) {
   console.log(`Connected to ${process.env.MYSQL_HOST} as user ${process.env.MYSQL_USERNAME}!`);
 });
 
-// Start API
+// API
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+
+// Create password
+const crypto = require('crypto');
+const prompt = require("prompt-sync")({ sigint: true });
+var fs = require('fs');
+
+if (process.env.API_PASSWORD === undefined) {
+  const getHashedPassword = (password) => {
+      const sha256 = crypto.createHash('sha256');
+      const hash = sha256.update(password).digest('base64');
+      return hash;
+  }
+  const setApiPassword = getHashedPassword(prompt("Enter a random seed to generate your password: "))
+  console.log(`Your API password is ${setApiPassword}`);
+  fs.appendFile('.env', `\nAPI_PASSWORD="${setApiPassword}"`, function (err) {
+    if (err) {
+      console.log(err)
+    }
+  })
+}
 
 // Allows cross origin requests
 const cors = require('cors');
 app.use(cors({ origin: true }));
 
-
+// Middleware
 app.use(express.json());
-
+app.use(cookieParser());
 
 app.post('/test', (req, res) => {
   res.status(200).send({ received_body: req.body });
